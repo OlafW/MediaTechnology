aa48c07db0b0d703a50ac81cb633c15e461a2891
Olaf Wisselink <olafwisselink@gmail.com>
2020-10-18 13:24:08 +0200

    mutate, fitness update

---
+++ computational_creativity/greatSnelliusBakeOff/code/recipe_util.py
@@ -0,0 +1,53 @@
+# import json
+
+def convert_to_ml(recipes):
+    for recipe in recipes:
+        ingredients = recipe['ingredients']
+
+        for ingredient in ingredients:
+            unit = ingredient['unit']
+            amount = ingredient['amount']
+
+            if (unit == "cup"):
+                amount *= 236.6
+            elif (unit == "tsp"):
+                amount *= 4.9
+            elif (unit == "tbsp"):
+                amount *= 14.8
+            elif (unit == "egss"):
+                amount *= 45
+
+            ingredient['amount'] = amount
+
+    return recipes
+
+def convert_from_ml(recipes):
+    for recipe in recipes:
+        ingredients = recipe['ingredients']
+
+        for ingredient in ingredients:
+            unit = ingredient['unit']
+            amount = ingredient['amount']
+
+            if (unit == "cup"):
+                amount /= 236.6
+            elif (unit == "tsp"):
+                amount /= 4.9
+            elif (unit == "tbsp"):
+                amount /= 14.8
+            elif (unit == "egss"):
+                amount /= 45
+
+            ingredient['amount'] = amount
+
+    return recipes
+
+
+def normalise_ml(recipes, norm_cookies=10):
+  for recipe in recipes:
+    ingredients = recipe['ingredients']
+
+    for ingredient in ingredients:
+      ingredient['amount'] *= norm_cookies / recipe['num_cookies']
+
+  return recipes
